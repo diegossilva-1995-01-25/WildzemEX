3423b4946e6b47410450c76dcefdbdfbecde39e9
Caio <caionpf@bitbucket.org>
2017-11-11 17:39:34 -0200

    LoginController rework

--- src/main/java/wildzem/controller/LoginController.java
+++ src/main/java/wildzem/controller/LoginController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wildzem.controller;
 
 import java.io.Serializable;
@@ -19,82 +14,93 @@ import wildzem.utils.CommonOperations;
 /**
  *
  * @author Caio
+ * @version 0.5
+ * 
+ * Managed bean. It's only function is to control login sessions in the system;
+ * 
  */
-
 @SessionScoped
 @Named(value="loginController")
 public class LoginController implements Serializable {
     private static final long serialVersionUID = 8999662889144586748L;
     
     @Inject private Staff staff;
+    private Staff sessionStaff;
     private StaffDAO dao;
     
     /**
-     * Creates a new instance of StaffBean
+     * Creates a new instance of LoginController
      */
     public LoginController() {
     }
     
+    /**
+     * After creating the instance this method is called to make an instance of the DAO
+     */
     @PostConstruct
     public void init() {
         
         dao = new StaffDAO();
     }
-    
-    //////////////////
-
-    public Staff getStaff() {
-        return staff;
-    }
-    public void setStaff(Staff staff) {
-        this.staff = staff;
-    }
-   
-    //////////////////
-    
-    public Staff getStaffSession() {
-        
-        FacesContext context = FacesContext.getCurrentInstance();
-        return (Staff) context.getExternalContext().getSessionMap().get("authUser");
-    }
-    
+      
+    /**
+     * Method to return to index.xhtml when inside the system
+     * 
+     * @return String redirecting to index.xhtml
+     */
     public String logOut() {
         
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
         
         return "/index.xhtml?faces-redirect=true";
     }
-    
+      
+    /**
+     * Validation method to grant access to the system.
+     * 
+     * @return String redirecting to home.xhtml
+     */
     public String logIn() {
         
         if ( StringUtils.isEmpty( staff.getUser() ) || StringUtils.isBlank( staff.getUser() ) ) {
             
+            // If the login is empty or blank throw message and do nothing       
             CommonOperations.message("Username is required!");
+            
             return null;
         }
         else if ( StringUtils.isEmpty( staff.getPassword() ) || StringUtils.isBlank( staff.getPassword() ) ) {
             
+            // If the password is empty or blank throw message and do nothing      
             CommonOperations.message("Password is required!");
+            
             return null;
         }
         else {
             
-            Staff s = dao.validateLogin(staff);
+            // Using the dao to select the user if it exists.   
+            sessionStaff = dao.validateLogin(staff);
             
-            if (s != null) {
-
-                staff.setName(s.getName());
-                staff.setUser(s.getUser());
-                staff.setPassword(null);
+            if (sessionStaff != null) {
+                
+                /* 
+                 * If the user exists set pass to null and add it to the session scope
+                 * Then redirect to home
+                 */
+                sessionStaff.setPassword(null);
+                staff = new Staff();
                 
                 FacesContext context = FacesContext.getCurrentInstance();
                 
-                context.getExternalContext().getSessionMap().put("authUser", staff);
+                context.getExternalContext().getSessionMap().put("authUser" + sessionStaff.getId(), sessionStaff);
                 
                 return "system/home?faces-redirect=true";
             }
             else {
-
+                
+                /* 
+                 * Else throw message and do nothing
+                 */
                 CommonOperations.message("Username or password is incorrect!");
                 return null;
             }
@@ -103,4 +109,24 @@ public class LoginController implements Serializable {
         
     }
     
+    /*
+     * Getter and setter section
+     */
+    public Staff getStaff() {
+        return staff;
+    }
+    public void setStaff(Staff staff) {
+        this.staff = staff;
+    }
+    
+    /**
+     * This method is used to get a user already in a session
+     * 
+     * @return Staff in session
+     */
+    public Staff getStaffSession() {
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        return (Staff) context.getExternalContext().getSessionMap().get("authUser" + sessionStaff.getId());
+    }
 }
